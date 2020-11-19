fe22b7c1e0fe2f6da1f7ab108317a9333c9de862
노병국 <raynear@gmail.com>
2020-11-20 00:36:57 +0900

    read python code from db

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import os
 import signal
+import pymysql
+from dotenv import load_dotenv
+
 from RestrictedPython import compile_restricted
 from RestrictedPython import Eval
 from RestrictedPython import utility_builtins
@@ -7,23 +11,39 @@ from RestrictedPython import utility_builtins
 # from RestrictedPython import limited_builtins
 # from RestrictedPython import Guards
 
+load_dotenv()
+
+db_pw = os.getenv("DB_PASSWORD")
+
+logic_db = pymysql.connect(user="root", passwd=db_pw, host="127.0.0.1", db="algotrade", charset="utf8")
+
+cursor = logic_db.cursor(pymysql.cursors.DictCursor)
+
+sql = "SELECT * FROM logic where user=1 limit 1;"
+cursor.execute(sql)
+result = cursor.fetchall()
+
+print(result)
+
 name = "raynear"
 myList = ["ray", "near", "cute", "yun", "suho"]
 loc = {}
-
-f = open("./logic1.py")
-byte_code = compile_restricted(f.read(), "<inline code>", "exec", None)
-f.close()
 
 
 # config and inject data and function
 global_builtins = {"__builtins__": utility_builtins}.copy()
 global_builtins["_getiter_"] = Eval.default_guarded_getiter
+global_builtins["_getattr_"] = getattr
+global_builtins["getattr"] = getattr
+global_builtins["_setattr_"] = setattr
+global_builtins["setattr"] = setattr
+global_builtins["_print_"] = print
 global_builtins["str"] = str
 global_builtins["name"] = name
 global_builtins["myList"] = myList
 # global_builtins["_iter_unpack_sequence_"] = Guards.guarded_iter_unpack_sequence
 
+byte_code = compile_restricted(result[0]["content"], "<inline code>", "exec", None)
 
 ### time out
 class TimeOutException(Exception):
